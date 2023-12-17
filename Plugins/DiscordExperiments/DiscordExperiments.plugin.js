d2b91b01de710e791551a4e58f7ec75554798d2d
CapnKitten <4013216+CapnKitten@users.noreply.github.com>
2023-12-16 22:49:37 -0500

    Update DiscordExperiments.plugin.js

--- Plugins/DiscordExperiments/DiscordExperiments.plugin.js
+++ Plugins/DiscordExperiments/DiscordExperiments.plugin.js
@@ -74,13 +74,13 @@ module.exports = (() => {
 		const plugin = (Plugin, Api) => {
 			return class DiscordExperiments extends Plugin {
 				onStart() {
-					let c = webpackChunkdiscord_app.push([[Symbol()],{},({c})=>Object.values(c)]);
-					let u = c.find((x)=> x?.exports?.default?.getUsers).exports.default;
+					let c; webpackChunkdiscord_app.push([[Symbol()],{},r=>c=r.c]); webpackChunkdiscord_app.pop();
+					let u = Object.values(c).find(x=>x?.exports?.default?.getUsers).exports.default;
 					let m = Object.values(u._dispatcher._actionHandlers._dependencyGraph.nodes);
 
 					u.getCurrentUser().flags |= 1;
 					m.find((x)=>x.name === "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]();
-					try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {}
+					try {m.find((x)=>x.name === "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user:{flags: 1}})} catch {};
 					m.find((x)=>x.name === "ExperimentStore").storeDidChange();
 				}
 
